21bace5893ab1b4b9fbc8d6b3184ea63b58f5a70
NPerlaza <161271265+NPerlaza@users.noreply.github.com>
2024-06-18 22:52:12 -0400

    Added routes and loaded data from database

---
+++ app.py
@@ -0,0 +1,48 @@
+# Importing all the good stuff
+from flask import Flask, jsonify, render_template_string
+from sqlalchemy import create_engine
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
+
+# Setting up my Flask app
+app = Flask(__name__)
+
+# Let's get that database connected
+engine = create_engine('sqlite:///social_media_data.db')
+
+# Time to load up the data when the app starts
+df1 = pd.read_sql_query("SELECT * FROM us_social_media", engine)
+df2 = pd.read_sql_query("SELECT * FROM google_trends", engine)
+
+# Here's my route for the US social media data
+@app.route("/api/v1.0/us_social_media")
+def us_social_media():
+    # Converting the DataFrame to a dictionary
+    data = df1.to_dict(orient='records')
+    # Sending it off as JSON
+    return jsonify(data)
+
+# And here's my route for the Google trends data
+@app.route("/api/v1.0/google_trends")
+def google_trends():
+    # Again, converting the DataFrame to a dictionary
+    data = df2.to_dict(orient='records')
+    # Off it goes as JSON
+    return jsonify(data)
+
+# Now, let's make a pretty plot
+@app.route("/plot")
+def plot():
+    # Creating a line plot with Plotly Express
+    fig = px.line(df1, x='Date', y='Facebook')
+    
+    # Converting the plot to HTML
+    plot_html = pio.to_html(fig, full_html=False)
+    
+    # Sending the plot as HTML
+    return render_template_string("<html><body>{{plot}}</body></html>", plot=plot_html)
+
+# Time to run the app!
+if __name__ == '__main__':
+    app.run(debug=True)
